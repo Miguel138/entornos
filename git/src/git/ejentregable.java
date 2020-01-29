b809ff7984eea88480e269344d7d5126f5df02a0
Miguel138 <60186370+Miguel138@users.noreply.github.com>
2020-01-29 16:01:32 +0100

    Update ejentregable.java
    cambio realizado desde el repositorio en linea

--- git/src/git/ejentregable.java
+++ git/src/git/ejentregable.java
@@ -2,8 +2,8 @@ package git;
 
 public class ejentregable {
 	public static void main(String[] args) {
-		for (int i = 0; i < 3; i++) {
-			System.out.print("Hola");
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Hola gente");
 		}
 	 
 }
